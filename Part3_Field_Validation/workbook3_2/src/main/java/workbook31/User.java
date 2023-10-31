6cf086867637b88a5426876188edf7edd0b118a9
Joe <109426792+JoeYeungCW@users.noreply.github.com>
2023-11-01 01:36:28 +0800

    Add files via upload

---
+++ Part3_Field_Validation/workbook3_2/src/main/java/workbook31/User.java
@@ -0,0 +1,93 @@
+package workbook31;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class User {
+    @NotBlank(message = "First name cannot be blank")
+    @Size(min = 2, message = "First name is too short")
+    private String firstName;
+
+    @NotBlank(message = "Last name cannot be blank")
+    @Size(min = 2, message = "Last name is too short")
+    private String lastName;
+
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 7, message = "Username is too short")
+    private String userName;
+
+    @Email(message = "Invaild Email")
+    private String email;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past(message = "date of birth must be in the past")
+    private Date dateOfBirth;
+
+    private String id;
+
+    public User() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public User(String firstName, String lastName, String userName, String email, Date dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
